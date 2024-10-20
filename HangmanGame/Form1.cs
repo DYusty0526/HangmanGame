21c6a10f9c6ac538943e61ecd79a9fe59bfd1473
DYusty0526 <140427282+DYusty0526@users.noreply.github.com>
2024-10-19 21:04:07 -0600

    commit

--- HangmanGame/Form1.cs
+++ HangmanGame/Form1.cs
@@ -27,6 +27,25 @@ namespace HangmanGame
         private void NewGame()
         {
             secretWord = wordList[random.Next(wordList.Length)];
+
+            displayWord = new string('_', secretWord.Length).ToCharArray();
+
+            remainingGuesses = 6;
+
+            incorrectGuesses.Clear();
+
+            UpdateGame();
         }
+        private void UpdateGame()
+        {
+            //This Line will display the given word as blanks and will display correct letters
+            lbl_Word.Text = string.Join(" ", displayWord);
+
+            lbl_Incorrect.Text = "Incorrect Guesses: " + string.Join(", ", incorrectGuesses);
+
+            lbl_Guesses.Text = "Guesses Left: " + remainingGuesses;
+
+        }
+
     }
 }
